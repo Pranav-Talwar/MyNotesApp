85191a052e0d167625442493fd942af2fd361618
Pranav-Talwar <131601400+Pranav-Talwar@users.noreply.github.com>
2025-02-12 02:00:46 -0700

    Delete NotesApp/NotesApp directory

--- NotesApp/NotesApp/src/main/java/com/example/NotesApp/repository/NoteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.NotesApp.repository;
-
-import com.example.NotesApp.model.Note;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface NoteRepository extends MongoRepository<Note, String> {
-}
